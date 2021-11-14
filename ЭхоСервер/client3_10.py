4faf50dceaf81bdc65c406c846f74a6f06daaac3
anna01122002 <76559002+anna01122002@users.noreply.github.com>
2021-11-14 18:35:46 +0300

    Add files via upload

---
+++ ЭхоСервер/client3_10.py
@@ -0,0 +1,50 @@
+import socket
+from time import sleep
+from threading import Thread
+
+
+def check():
+	while True:
+		try:
+			data = sock.recv(1024)
+			msg = data.decode()
+			print(msg)
+		except:
+			break
+
+
+try:
+	sock = socket.socket()
+	print('kakoy host?')
+	x = input()
+	print('kakoy port?')
+	y = int(input())
+	sock.connect((x, y))
+except:
+	x = 'localhost'
+	y = 9091
+	y = 9095
+	sock.connect((x, y))
+
+msg = ''
+print("vvedite parol")
+passwd = input()
+msg = passwd
+sock.send(msg.encode())
+
+Thread(target=check).start()
+
+while msg !="close":
+	msg = input()
+	if msg !='close':
+		sock.send(msg.encode())
+
+
+
+
+msg = 'client disconnected'
+sock.send(msg.encode())
+
+data = sock.recv(1024)
+
+sock.close()
